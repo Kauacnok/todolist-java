2423857c4ab333ffeda492db09c0ccc023c3925c
Kauacnok <kauaprofessionalcontact@gmail.com>
2023-10-10 22:03:06 -0300

    integrating the database into the project - java class 02

--- src/main/java/br/com/kauacnok/todolist/user/UserController.java
+++ src/main/java/br/com/kauacnok/todolist/user/UserController.java
@@ -2,17 +2,31 @@ package br.com.kauacnok.todolist.user;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import br.com.kauacnok.todolist.user.UserModel;
+import br.com.kauacnok.todolist.user.IUserRepository;
 
 @RestController()
 @RequestMapping("/user")
 public class UserController {
 
+	@Autowired
+	private IUserRepository userRepository;
+
 	@PostMapping("/createUser")
-	public void create(@RequestBody UserModel userModel) {
-		System.out.println(userModel.name);
+	public ResponseEntity create(@RequestBody UserModel userModel) {
+		var user = this.userRepository.findByUsername(userModel.getUsername());
+
+		if (user != null) {
+			System.out.println("Usuário já existe");
+			return ResponseEntity.status(400).body("Usuário já existe");
+		}
+
+		var userCreated = this.userRepository.save(userModel);
+		return ResponseEntity.status(201).body(userCreated);
 	}
 
 }
--- src/main/java/br/com/kauacnok/todolist/user/UserModel.java
+++ src/main/java/br/com/kauacnok/todolist/user/UserModel.java
@@ -1,14 +1,30 @@
 package br.com.kauacnok.todolist.user;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.UUID;
+import java.time.LocalDateTime;
 
-@RestController()
-@RequestMapping("/users")
+import org.hibernate.annotations.CreationTimestamp;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Column;
+
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_users")
 public class UserModel {
 
+	@Id
+	@GeneratedValue(generator = "UUID")
+	private UUID id;
+
+	@Column(unique = true)
 	private String username;
 	private String name;
 	private String password;
+
+	@CreationTimestamp
+	private LocalDateTime created_at;
 
 }
